17a9ae57814b13f02cba06655c2f44b87775b948
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-06-22 07:50:38 +0300

    Display Products List In Cards (UI)

---
+++ frontend/src/productsList.jsx
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpds Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit arcu ultricies penatibus torquent fusce, varius mus pretium sed morbi magnis tempus vestibulum nullam tincidunt tellus.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "IPhone 11 pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Natoque laoreet himenaeos sed libero auctor torquent neque mollis proin pellentesque nostra arcu gravida nulla, risus sagittis scelerisque curae faucibus fames sollicitudin parturient tempor hac ac nascetur.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Torquent himenaeos viverra per tincidunt turpis phasellus primis, congue cras commodo vulputate nibh montes non, tortor sociosqu dis facilisi sociis facilisis. Ultricies ante fermentum ultrices non mauris sagittis congue etiam molestie, inceptos tincidunt facilisis purus torquent per senectus turpis, interdum sociis mi vel libero habitant elementum netus.",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Sony Playstation 4 Pro White Version",
+    image: "/images/playstation.jpg",
+    description:
+      "Torquent himenaeos viverra per tincidunt turpis phasellus primis, congue cras commodo vulputate nibh montes non, tortor sociosqu dis facilisi sociis facilisis. Ultricies ante fermentum ultrices non mauris sagittis congue etiam molestie, inceptos tincidunt facilisis purus torquent per senectus turpis, interdum sociis mi vel libero habitant elementum netus.",
+    brand: "Sony",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "primis est at ridiculus vivamus, montes potenti odio venenatis litora nibh eget molestie. Congue lacus est nulla fringilla bibendum risus cursus facilisi tempus, magna vel commodo laoreet consequat per cum.",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Amazon Echo 3rd Generation",
+    image: "/images/alexa.jpg",
+    description:
+      "metus libero viverra ultricies sapien. Praesent fames blandit torquent lacinia a placerat pellentesque habitasse, tempus donec velit mattis aliquam sodales sociosqu, conubia facilisi dignissim duis accumsan vehicula primis.",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
