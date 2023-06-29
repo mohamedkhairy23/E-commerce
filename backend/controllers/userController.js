2cff189f39782f3e8bf7e8d4d67ddf0740a6ad26
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-06-29 05:04:21 +0300

    User routes and controllers setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+// @desc      Auth user & get token
+// @route     POST /api/users/login
+// @access    Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login user");
+});
+
+// @desc      Register user
+// @route     POST /api/users
+// @access    Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("Register user");
+});
+
+// @desc      Logout user / clear cookie
+// @route     POST /api/users/logout
+// @access    Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("Logout user");
+});
+
+// @desc      Get user profile
+// @route     GET /api/users/profile
+// @access    Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("Get user profile");
+});
+
+// @desc      Update user profile
+// @route     PUT /api/users/profile
+// @access    Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("Update user profile");
+});
+
+// @desc      Get users
+// @route     GET /api/users
+// @access    Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("Get users");
+});
+
+// @desc      Get user
+// @route     GET /api/users/:id
+// @access    Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("Get user by ID");
+});
+
+// @desc      Delete user
+// @route     DELETE /api/users/:id
+// @access    Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("Delete user");
+});
+
+// @desc      Update user
+// @route     UPDATE /api/users/:id
+// @access    Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("Update user");
+});
+
+export {
+  loginUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectToDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
 app.use(notFound);
