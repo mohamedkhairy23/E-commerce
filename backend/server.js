43a0cbb44cec98ca47381ce350e098e5c55d0b2f
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-06-29 06:55:36 +0300

    User email and password validation

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,10 @@ connectToDB();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
