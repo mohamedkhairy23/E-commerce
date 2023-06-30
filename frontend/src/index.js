1276c29fecd50d190e37be439bda94827129c7c5
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-06-30 11:31:37 +0300

    Register reducer and functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,7 +25,8 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />{" "}
-      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/login" element={<LoginScreen />} />{" "}
+      <Route path="/register" element={<RegisterScreen />} />
       <Route />
     </Route>
   )
