97d84d3a828461c6d330b16b1e0675ee3560a5b2
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-07-07 16:25:56 +0300

    Users list for admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -30,6 +30,7 @@ import AdminRoute from "./components/AdminRoute";
 import OrderListScreen from "./screens/Admin/OrderListScreen";
 import ProductListScreen from "./screens/Admin/ProductListScreen";
 import ProductEditScreen from "./screens/Admin/ProductEditScreen";
+import UserListScreen from "./screens/Admin/UserListScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -50,6 +51,7 @@ const router = createBrowserRouter(
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
         <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+        <Route path="/admin/userlist" element={<UserListScreen />} />
       </Route>
     </Route>
   )
